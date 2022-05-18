909d38a93fdc2b659c7d9070c42671957f0d73dc
GenFusion122 <infusion122@hotmail.com>
2022-05-18 22:06:54 +0800

    Skeleton done

--- beecheal/lib/screens/home/home.dart
+++ beecheal/lib/screens/home/home.dart
@@ -1,11 +1,57 @@
+import 'package:beecheal/screens/home/calendar.dart';
+import 'package:beecheal/screens/home/journalEntries.dart';
+import 'package:beecheal/screens/home/statistics.dart';
+import 'package:beecheal/screens/wrapper.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Text('home'),
-    );
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Home!'),
+        centerTitle: true,
+        backgroundColor: Colors.orange,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.orange)),
+                  child: Text("statistics"),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => Statistics()));
+                  }),
+              ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.orange)),
+                  child: Text("calendar"),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => calendar()));
+                  }),
+              ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.orange)),
+                  child: Text("journal"),
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => journalEntries()));
+                  })
+            ],
+          ),
+        ],
+      ),
+    ));
   }
 }
---
+++ beecheal/lib/screens/home/statistics.dart
@@ -0,0 +1,56 @@
+import 'package:beecheal/main.dart';
+import 'package:beecheal/screens/home/home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Statistics extends StatelessWidget {
+  const Statistics({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return (MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('stats skreen'),
+              centerTitle: true,
+              backgroundColor: Colors.orange,
+              actions: <Widget>[
+                Padding(
+                    padding: EdgeInsets.only(right: 20),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).push(
+                            MaterialPageRoute(builder: (context) => Home()));
+                      },
+                      child: Icon(
+                        Icons.home,
+                        size: 26.0,
+                      ),
+                    ))
+              ],
+            ),
+            body: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        ElevatedButton(
+                            style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                        Colors.orange)),
+                            onPressed: () {},
+                            child: Text("mood")),
+                        ElevatedButton(
+                            style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                        Colors.orange)),
+                            onPressed: () {},
+                            child: Text("productivity")),
+                      ]),
+                ]))));
+  }
+}
