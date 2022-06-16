61e03949d455fb606d5e630482ed2e2cf8280721
chia-yh <e0727341@u.nus.edu>
2022-06-16 14:07:17 +0800

    Revert "Revert "as prev commit""
    This reverts commit cb33b1655be9a0ed4ac9079812b449b6abfdd87a.

--- beecheal/macos/Flutter/GeneratedPluginRegistrant.swift
+++ beecheal/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,6 @@ import cloud_firestore
 import firebase_auth
 import firebase_core
 import firebase_messaging
-import firebase_ml_model_downloader
 import flutter_local_notifications
 import flutter_native_timezone
 
@@ -18,7 +17,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
-  FirebaseModelDownloaderPlugin.register(with: registry.registrar(forPlugin: "FirebaseModelDownloaderPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterNativeTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterNativeTimezonePlugin"))
 }
